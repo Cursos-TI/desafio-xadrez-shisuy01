a36cb66ecff57d132fc004323ad651ba2e9f6652
shisuy01 <cleitonaragorn@gmail.com>
2025-12-04 11:17:45 -0300

    Limpando e organizando o código
    Alteração de correção e sintaxe

--- xadrez.c
+++ xadrez.c
@@ -4,62 +4,60 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-    #include <stdio.h>
+    int main() {
 
-int main() {
+        int option;
 
-    int option;
+        // Desafio para Novato 
+        // Menu de comando das Peças 
+        printf("### MateCheck ###\n\n");
+        printf("Escolha a peça a ser movida: \n");
+        printf("1. Bispo\n");
+        printf("2. Torre\n");
+        printf("3. Rainha\n");
+        scanf("%d", &option);
 
-    // Desafio para Novato 
-    // Menu de comando das Peças 
-    printf("### MateCheck ###\n\n");
-    printf("Escolha a peça a ser movida: \n");
-    printf("1. Bispo\n");
-    printf("2. Torre\n");
-    printf("3. Rainha\n");
-    scanf("%d", &option);
+        // Código para identificar apenas as opções 1, 2 e 3
 
-    // Código para identificar apenas as opções 1, 2 e 3
+        if (option < 1 || option > 3) {
+            printf("Opção Inválida\n");
+            return 0;
+        }
 
-    if (option < 1 || option > 3) {
-        printf("Opção Inválida\n");
-        return 0;
-    }
-
-    // Código para a movimentação das peças 
+        // Código para a movimentação das peças 
     
-    switch (option)
-    {
-    case 1: // Bispo
-        printf("\nMovimentando Bispo\n");
-
-        for (int i = 0; i < 5; i++) 
+        switch (option)
         {
-            printf("Diagonal\n", i);
-        }
-        break;
+        case 1: // Bispo
+            printf("\nMovimentando Bispo\n");
 
-    case 2: // Torre
-        printf("\nMovimentando Torre\n");
+            for (int i = 0; i < 5; i++) 
+        {    
+                printf("Diagonal\n", i);
+            }
+            break;
 
-        for (int i = 0; i < 5; i++) 
-        {
-            printf("\nDireita", i);
-        }
-        break;
-    case 3: // Rainha
+        case 2: // Torre
+            printf("\nMovimentando Torre\n");
+
+            for (int i = 0; i < 5; i++) 
+            {
+                printf("\nDireita", i);
+            }
+            break;
+        case 3: // Rainha
     
-        for (int i = 0; i < 8; i++) 
-        {
-            printf("\nEsquerda", i);
+            for (int i = 0; i < 8; i++) 
+            {
+                printf("\nEsquerda", i);
+            }
+            break;
+        default:
+            break;
         }
-        break;
-    default:
-        break;
-    }
 
-     return 0;
-}
+         return 0;
+    }
     
 
     
@@ -86,5 +84,5 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
-}
+  
+
